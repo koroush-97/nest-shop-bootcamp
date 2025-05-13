0a399b3467455f5864a6b7d8bf7999e242484ca9
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-05-13 12:00:06 +0330

    Menu-tsx api was done

--- src/components/layout/header/menu/Menu.tsx
+++ src/components/layout/header/menu/Menu.tsx
@@ -11,6 +11,7 @@ import { IconBox } from "@/components/common";
 import { menuMock } from "@/mock/menu";
 import { useQuery } from "@tanstack/react-query";
 import { getMenuApiCall } from "@/api/menu/Menu";
+import { EntityType, MenuItemType, menuType, PopulateType } from "@/types";
 
 export function Menu() {
   // TODO load menu data from api
@@ -21,6 +22,55 @@ export function Menu() {
   });
 
   console.log("menuData :", menuData);
+
+  let mainMenuItems: null | PopulateType<MenuItemType> = null;
+  let categoryMenuItems: null | PopulateType<MenuItemType> = null;
+
+  if (menuData) {
+    const findMenu = menuData.data.filter(
+      (item: EntityType<menuType>) => item.attributes.position === "main_menu"
+    );
+    if (findMenu.length > 0) {
+      mainMenuItems = findMenu[0].attributes.menu_items;
+      mainMenuItems.data.sort(
+        (a: EntityType<MenuItemType>, b: EntityType<MenuItemType>) => {
+          if (a.attributes.rank < b.attributes.rank) {
+            return -1;
+          }
+
+          if (a.attributes.rank > b.attributes.rank) {
+            return 1;
+          }
+
+          return 0;
+        }
+      );
+    }
+  }
+
+  // ////////////////////
+  if (menuData) {
+    const findCaregoryMenu = menuData.data.filter(
+      (item: EntityType<menuType>) =>
+        item.attributes.position === "brows-category"
+    );
+    if (findCaregoryMenu.length > 0) {
+      categoryMenuItems = findCaregoryMenu[0].attributes.menu_items;
+      categoryMenuItems.data.sort(
+        (a: EntityType<MenuItemType>, b: EntityType<MenuItemType>) => {
+          if (a.attributes.rank < b.attributes.rank) {
+            return -1;
+          }
+
+          if (a.attributes.rank > b.attributes.rank) {
+            return 1;
+          }
+
+          return 0;
+        }
+      );
+    }
+  }
 
   return (
     <>
@@ -35,7 +85,6 @@ export function Menu() {
           link={"#"}
           titleClassName={"text-medium ml-1"}
         />
-
         <IconBox icon={"icon-angle-small-down"} size={24} />
 
         <div
@@ -46,7 +95,7 @@ export function Menu() {
             id="all_cat_inner_box"
             className="flex flex-wrap justify-between gap-y-[15px]"
           >
-            {browserCategoryMock.map((item, index) => {
+            {/* {browserCategoryMock.map((item, index) => {
               return (
                 <IconBox
                   key={index}
@@ -61,7 +110,27 @@ export function Menu() {
                   path={item.iconPath}
                 />
               );
-            })}
+            })} */}
+
+            {categoryMenuItems &&
+              categoryMenuItems.data.map(
+                (item: EntityType<MenuItemType>, index: number) => {
+                  return (
+                    <IconBox
+                      key={index}
+                      linkClassName={
+                        "flex items-center gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5 basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300 "
+                      }
+                      link={item.attributes.link}
+                      icon={item.attributes.icon_name}
+                      size={30}
+                      title={item.attributes.title}
+                      titleClassName={"text-heading-sm text-blue-300"}
+                      path={item.attributes.icon_path}
+                    />
+                  );
+                }
+              )}
 
             <div
               id="more_categories"
@@ -78,19 +147,30 @@ export function Menu() {
 
       <nav id="main_menu">
         <ul className="flex flex-col lg:flex-row items-start lg:items-center text-heading6 lg:text-heading-sm 2xl:text-heading6 gap-[32px] mt-[32px] lg:mt-0 lg:gap-3 xl:gap-5 2xl:gap-10">
-          {menuMock.map((item, index) => {
-            return (
-              <li key={index}>
-                {item.icon ? (
-                  <IconBox {...item} size={24} />
-                ) : (
-                  <Link href={item.link} className="flex items-center gap-1">
-                    {item.title}
-                  </Link>
-                )}
-              </li>
-            );
-          })}
+          {mainMenuItems &&
+            mainMenuItems.data.map(
+              (item: EntityType<MenuItemType>, index: number) => {
+                return (
+                  <li key={index}>
+                    {item.attributes.icon_name ? (
+                      <IconBox
+                        link={item.attributes.link}
+                        icon={item.attributes.icon_name}
+                        title={item.attributes.title}
+                        size={24}
+                      />
+                    ) : (
+                      <Link
+                        href={item.attributes.link}
+                        className="flex items-center gap-1"
+                      >
+                        {item.attributes.title}
+                      </Link>
+                    )}
+                  </li>
+                );
+              }
+            )}
         </ul>
       </nav>
     </>
