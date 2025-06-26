e39df779089016d497cf676313d5ff2a2c8f60a8
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-06-26 07:48:16 +0330

    Contact us form has been created

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -1,9 +1,27 @@
 import { MainTitlesType1, MainTitlesType2 } from "@/mock/titleCards";
 // components
-import { TitleCard, Section, ServicesCard, ImagView } from "@/components";
+import {
+  TitleCard,
+  Section,
+  ServicesCard,
+  ImagView,
+  Input,
+  TextArea,
+} from "@/components";
 import { ServiceCardMock, compareShopMock } from "../../mock/ServiceCard";
+import { useForm } from "react-hook-form";
 
 export default function ContactUS() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    console.log("data");
+  };
+
   return (
     <>
       <Section classname="min-h-[356px] p-2  flex flex-col md:justify-center md:items-center md:flex-row gap-0 md:gap-10">
@@ -23,7 +41,7 @@ export default function ContactUS() {
 
       <Section classname="h-[483px] border-2">test</Section>
 
-      <Section classname="min-h-[213px]  border-red-500 flex flex-col md:flex-row gap-10">
+      <Section classname="min-h-[213px]   flex flex-col md:flex-row gap-10">
         <ServicesCard
           data={compareShopMock[0]}
           className="md:w-[33.33%]  flex flex-col h-auto p-2"
@@ -41,25 +59,76 @@ export default function ContactUS() {
         />
       </Section>
 
-      <Section classname="min-h-[697px] border-2 p-2 flex flex-col md:flex-row">
-        <div
-          className="w-full md:w-[66.33%] flex flex-col "
-          style={{ border: "2px solid red" }}
-        >
-          <div className="h-1/3" style={{ border: "2px solid blue" }}>
+      <Section classname="min-h-[697px]  p-2 flex flex-col md:flex-row">
+        <div className="w-full md:w-[66.33%] flex flex-col ">
+          <div className="h-1/3">
             <TitleCard
               data={MainTitlesType2}
               className="w-full   flex flex-col"
             />
           </div>
-          <div className="h-2/3" style={{ border: "2px solid black" }}>
-            2
+          <div className="h-2/3 gap-1 flex flex-col ">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="min-h-[135px] flex flex-col p-2 md:flex-row gap-5">
+                <div className="flex flex-col w-full  md:w-1/2 gap-5">
+                  <Input
+                    type="text"
+                    register={register("your-name", {
+                      required: "Please write your name",
+                    })}
+                    {...{ placeholder: "Your Name *" }}
+                    errors={errors}
+                  />
+                  <Input
+                    type="tel"
+                    register={register("Email", {
+                      required: "Please write your phone number",
+                    })}
+                    {...{ placeholder: "phone Number *" }}
+                    errors={errors}
+                  />
+                </div>
+                <div className=" flex flex-col w-full  md:w-1/2 gap-5">
+                  <Input
+                    type="email"
+                    register={register("email", {
+                      required: "Please write your Email ",
+                    })}
+                    {...{ placeholder: "Email *" }}
+                    errors={errors}
+                  />
+                  <Input
+                    type="text"
+                    register={register("subject", {
+                      required: "you should fill this input",
+                    })}
+                    {...{ placeholder: "subject *" }}
+                    errors={errors}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <TextArea
+                  placeholder="Your message *"
+                  register={register("message", {
+                    required: "Please write your message",
+                  })}
+                  errors={errors}
+                />
+              </div>
+              <div className="p-3 w-[191px] h-[61px] flex justify-center items-center">
+                <button
+                  className="bg-[#253D4E] flex justify-center items-center p-6 text-[#fff] rounded-md text-[14px] w-full h-full"
+                  type="submit"
+                >
+                  Send message
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <div
-          className=" hidden md:flex md:w-[33.33%] justify-center items-center"
-          style={{ border: "2px solid red" }}
-        >
+        <div className=" hidden md:flex md:w-[33.33%] justify-center items-center">
           <ImagView
             src="/assets/images/contactus/girlwithphone.jpg"
             width={378}
